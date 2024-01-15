c2793c4597106285a11c05585917f84ddfa0043b
Seyeon <seyeon47@naver.com>
2024-01-16 01:10:13 +0900

    feat: collaborative filtering base serving code

--- src/app/recommend/src/main.py
+++ src/app/recommend/src/main.py
@@ -3,25 +3,39 @@ from src.api.recommend_api import recommendationAPIRouter
 from uvicorn import run
 import configparser
 
-app = FastAPI()
 
+IF_PYCHARM = True
+
+app = FastAPI()
 config = configparser.ConfigParser()
-config.read("config.env")
+
+if IF_PYCHARM:
+    config.read("../config.env")
+else:
+    config.read("config.env")
 
 server = config["server"]
 server_port = int(server["port"])
 
+
 def start():
     run(app, port=server_port)
+
 
 @app.get("/test")
 def test(text: str = "test"):
     return recommendationAPIRouter.test(text)
 
+
 @app.get("/collabo/filters")
-def test(product_name: str):
+async def recommend_product(product_name: str):
     return recommendationAPIRouter.recommend_product(product_name=product_name)
+
 
 @app.get("/product")
 def test(id: int):
     return recommendationAPIRouter.product_info(product_id=id)
+
+
+if __name__ == '__main__':
+    run(app, host='0.0.0.0', port=server_port)
