ad24151754b7fcf546be33a1b850b17cd4c02276
ooooasisss <rkdud104704@gmail.com>
2024-01-16 12:47:55 +0900

    modified ui, search

--- src/app/front/src/pages/🔠 service_info.py
+++ src/app/front/src/pages/🔠 service_info.py
@@ -1,17 +1,11 @@
 import streamlit as st
 
-# st.set_page_config(
-#     page_title="Hello",
-#     page_icon="👋",
-# )
-
-st.write("# Welcome to Streamlit! 👋")
+st.title("🔠 서비스소개")
 
 st.markdown(
     """
     <서비스명>은 기초화장품을 고르는데에 어려움을 겪는 모든 이들을 위한 맞춤형 챗봇서비스입니다.
     **👈 사이드바에서 <서비스>와 <분석보고서>** 를 골라서 도움을 받아보세요!
-    of what Streamlit can do!
     ### 팀소개
     - 저희는 [새싹캠퍼스(용산)](https://sesac.seoul.kr)에서 
     [핀테커스](https://sesac.seoul.kr/course/active/detail.do?courseActiveSeq=1600&srchCategoryTypeCd=&courseMasterSeq=337&currentMenuId=900002001)
