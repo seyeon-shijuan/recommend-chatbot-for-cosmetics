2ecbbb570710ae0c95837858fb960984e8fcd7c4
statezeropy <--global>
2024-01-17 17:52:59 +0900

    update rag system

--- src/dataloader/rag/rag_make_chroma.py
+++ src/dataloader/rag/rag_make_chroma.py
@@ -3,24 +3,50 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 
 # load the document and split it into chunks
-loader = CSVLoader("cosmetic_dataset.csv", csv_args={
-  'delimiter': ',',
-  'quotechar': '"',
-  'fieldnames': ['nickname','제품명','종류','평점','피부타입','특징1','특징2','특징3','리뷰']
-})
-documents = loader.load()
+def csv_loader(filename, fieldnames):
+    loader = CSVLoader(filename, csv_args={
+    'delimiter': ',',
+    'quotechar': '"',
+    'fieldnames': fieldnames
+    })
+    documents = loader.load()
+    return documents
+# documents = csv_loader(doc[1], fieldnames[1])
 
+# load embeddings model
+def load_embeddings(model_id="jhgan/ko-sbert-nli"):
+    model_name = model_id
+    encode_kwargs = {'normalize_embeddings': True}
+    embedding = HuggingFaceEmbeddings(
+        model_name=model_name,
+        encode_kwargs=encode_kwargs
+    )
+    return embedding
 
-# VectorDB
-model_name = "jhgan/ko-sbert-nli"
-encode_kwargs = {'normalize_embeddings': True}
-ko_embedding = HuggingFaceEmbeddings(
-    model_name=model_name,
-    encode_kwargs=encode_kwargs
-)
 # save to disk: persist_directory="./chroma_db"
-vectorstore = Chroma.from_documents(documents=documents, embedding=ko_embedding, persist_directory="./chroma_db")
+def init_and_save_vectorstore(documents=csv_loader(), persist_directory="./chroma_db", embedding=load_embeddings()):
+    Chroma.from_documents(documents=documents, embedding=embedding, persist_directory=persist_directory)
 
 # load from disk
-# vectorstore = Chroma(persist_directory="./chroma_db", embedding=ko_embedding)
+def load_vectorstore(persist_directory="./chroma_db", embedding=load_embeddings()):
+    vectorstore = Chroma(persist_directory=persist_directory, embedding_function=embedding)
+    return vectorstore
 
+def add_vectorstore(vectorstore=load_vectorstore(), documents=csv_loader()) -> None:
+    vectorstore.add_texts(texts=[doc.page_content for doc in documents], 
+                          metadatas=[doc.metadata for doc in documents])
+    vectorstore.persist()
+
+
+persist_directory = "src/app/chatbot/resource/data/chroma_db"
+doc = ['cosmetic_dataset.csv', 'cosmetic_ingredients.csv']
+fieldnames = [['nickname','제품명','종류','평점','피부타입','특징1','특징2','특징3','리뷰'], ['성분명','기원 및 정의','배합목적']]
+
+
+# init and save vectorstore
+init_and_save_vectorstore()
+
+# add new vectorstore
+documents= csv_loader(doc[1], fieldnames[1])
+vectorstore = load_vectorstore()
+add_vectorstore(vectorstore=vectorstore, documents=documents)
